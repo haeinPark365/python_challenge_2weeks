4038777c7c34753a553a49f291b78e1e8035611f
HAEIN PARK <sunman365@naver.com>
2020-11-19 13:38:28 +0000

    python_2_week_challenge

--- day_four.py
+++ day_four.py
@@ -1,2 +1,6 @@
-import request
+import requests
 
+def run_day_four():
+  URL = "http://www.naver.com"
+  request = requests.get(URL)
+  print(request.status_code)
---
+++ main.py
@@ -0,0 +1,5 @@
+from day_three import run_day_three
+from day_four import run_day_four
+
+#run_day_three()
+run_day_four()
